e15fb44bf0463777bf3b137f3ad201c811775391
Adam Craig and Luca Quatela <luca@>
2016-05-25 14:48:05 -0700

    Add functionality for displaying information such as name, serial number, image, year, and stolen date when user searches a city.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,3 +1,7 @@
-Bike = function(name) {
+Bike = function(name, serial, thumb, year, date_stolen) {
   this.name = name;
+  this.serial = serial;
+  this.thumb = thumb;
+  this.year = year;
+  this.date_stolen = date_stolen;
 };
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -9,21 +9,38 @@ $(document).ready(function(){
     city = $('#cityInput').val();
     $('#cityInput').val("");
 
-    $('.output').text("The city you have chosen is " + city + ". ");
+    $('.output').text("Here's a list of all bikes reported stolen in " + city + " during the past week:");
+    bikes = [];
+    $('.loadingImage').show();
+    $('.bikeList').empty();
     $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + city + '&proximity_square=100&stolen_after=' + lastweek).then(function(response) {
       console.log(response);
+      $('.loadingImage').hide();
 
       response.bikes.forEach(function(bike) {
-        var newBike = new Bike(bike.frame_model);
+        var newBike = new Bike(bike.frame_model, bike.serial, bike.thumb, bike.year, bike.date_stolen);
         bikes.push(newBike);
       });
 
       bikes.forEach(function(bike) {
-        $('.bikeList').append('<li>' + bike.name + '</li>');
-        console.log(bike.name);
-      });
+        if (bike.thumb) {
+          $('.bikeList').append("<div class='col-md-4 bikeBox'>" + "Name: " + bike.name +
+                                "<br>" + "Serial#: " + bike.serial +
+                                "<br> <img src=" + bike.thumb + ">"  +
+                                "<br>" + "Year: " + bike.year +
+                                "<br>" + "Stolen Date: " + bike.date_stolen +
+                                "</div>");
+        } else {
+          $('.bikeList').append("<div class='col-md-4 bikeBox'>" + "Name: " + bike.name +
+                                "<br>" + "Serial #: " + bike.serial +
+                                "<br> <img src='img/bike.png'>" +
+                                "<br>" + "Year: " + bike.year +
+                                "<br>" + "Stolen Date: " + bike.date_stolen +
+                                "</div>");
+        }
 
-      $('.output').show();
+
+      });
 
     });
 
